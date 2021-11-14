eade18b12214a7175f6cf79fe89088cb98b5eaa4
teagueStockwell <tsAppDevelopment@gmail.com>
2021-11-14 05:48:26 -0800

    Remove static simple icons imports

--- constants/iconTags.js
+++ constants/iconTags.js
@@ -1,124 +1,85 @@
-import jsIcon from "simple-icons/icons/javascript";
-import typescriptIcon from "simple-icons/icons/typescript";
-import reactIcon from "simple-icons/icons/react";
-import reduxIcon from "simple-icons/icons/redux";
-import reactrouterIcon from "simple-icons/icons/reactrouter";
-import nextdotjsIcon from "simple-icons/icons/nextdotjs";
-import vuedotjsIcon from "simple-icons/icons/vuedotjs";
-import jqueryIcon from "simple-icons/icons/jquery";
-import css3Icon from "simple-icons/icons/css3";
-import styledcomponentsIcon from "simple-icons/icons/styledcomponents";
-import sassIcon from "simple-icons/icons/sass";
-import antdesignIcon from "simple-icons/icons/antdesign";
-import tailwindcssIcon from "simple-icons/icons/tailwindcss";
-import bootstrapIcon from "simple-icons/icons/bootstrap";
-import materialuiIcon from "simple-icons/icons/materialui";
-import nodedotjsIcon from "simple-icons/icons/nodedotjs";
-import expressIcon from "simple-icons/icons/express";
-import nestjsIcon from "simple-icons/icons/nestjs";
-import amazonawsIcon from "simple-icons/icons/amazonaws";
-import nginxIcon from "simple-icons/icons/nginx";
-import vercelIcon from "simple-icons/icons/vercel";
-import dockerIcon from "simple-icons/icons/docker";
-import mongodbIcon from "simple-icons/icons/mongodb";
-import redisIcon from "simple-icons/icons/redis";
-import digitaloceanIcon from "simple-icons/icons/digitalocean";
-import herokuIcon from "simple-icons/icons/heroku";
-import githubactionsIcon from "simple-icons/icons/githubactions";
-import expoIcon from "simple-icons/icons/expo";
-import androidstudioIcon from "simple-icons/icons/androidstudio";
-import gitIcon from "simple-icons/icons/git";
-import visualstudiocodeIcon from "simple-icons/icons/visualstudiocode";
-import jiraIcon from "simple-icons/icons/jira";
-import figmaIcon from "simple-icons/icons/figma";
-import jestIcon from "simple-icons/icons/jest";
-import linuxIcon from "simple-icons/icons/linux";
-import sentryIcon from "simple-icons/icons/sentry";
-import githubIcon from "simple-icons/icons/github";
-import framerIcon from "simple-icons/icons/framer";
-
 export const stacks = ["frontend", "backend", "mobile", "tools"];
 
 export const frontEndTags = [
   {
     slug: "javascript",
     url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
-    icon: jsIcon,
+    
   },
   {
     slug: "typescript",
     url: "https://www.typescriptlang.org/",
-    icon: typescriptIcon,
+    
   },
   {
     slug: "react",
     url: "https://reactjs.org/",
-    icon: reactIcon,
+    
   },
   {
     slug: "redux",
     url: "https://redux.js.org",
-    icon: reduxIcon,
+    
   },
   {
     slug: "reactrouter",
     url: "https://reactrouter.com/",
-    icon: reactrouterIcon,
+    
   },
   {
     slug: "nextdotjs",
     url: "https://nextjs.org/",
-    icon: nextdotjsIcon,
+    
   },
   {
     slug: "vuedotjs",
     url: "https://vuejs.org/",
-    icon: vuedotjsIcon,
+    
   },
   {
     slug: "jquery",
     url: "https://jquery.com/",
-    icon: jqueryIcon,
+    
   },
   {
     slug: "css3",
     url: "https://www.w3schools.com/css/",
-    icon: css3Icon,
+    
   },
   {
     slug: "styledcomponents",
     url: "https://styled-components.com/",
-    icon: styledcomponentsIcon,
+    
   },
   {
     slug: "sass",
     url: "https://sass-lang.com/",
-    icon: sassIcon,
+    
   },
   {
     slug: "antdesign",
     url: "https://ant.design/",
-    icon: antdesignIcon,
+    
   },
   {
     slug: "tailwindcss",
     url: "https://tailwindcss.com/",
-    icon: tailwindcssIcon,
+    
   },
   {
     slug: "bootstrap",
     url: "https://getbootstrap.com/",
-    icon: bootstrapIcon,
+    
   },
   {
     slug: "materialui",
     url: "https://mui.com/",
-    icon: materialuiIcon,
+    
   },
   {
     slug: "framer",
     url: "https://framer.com/",
-    icon: framerIcon,
+    
   },
 ];
 
@@ -126,73 +87,73 @@ export const backendIconTags = [
   {
     slug: "javascript",
     url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
-    icon: jsIcon,
+    
   },
   {
     slug: "typescript",
     url: "https://www.typescriptlang.org/",
-    icon: typescriptIcon,
+    
   },
   {
     slug: "nodedotjs",
     url: "https://nodejs.org/en/",
-    icon: nodedotjsIcon,
+    
   },
   {
     slug: "express",
     url: "https://expressjs.com/",
-    icon: expressIcon,
+    
   },
   {
     slug: "nestjs",
     url: "https://nestjs.com/",
-    icon: nestjsIcon,
+    
   },
   {
     slug: "amazonaws",
     url: "https://aws.amazon.com/",
-    icon: amazonawsIcon,
+    
   },
   {
     slug: "nginx",
     url: "https://www.nginx.com/",
-    icon: nginxIcon,
+    
   },
   {
     slug: "vercel",
     url: "https://vercel.com/",
-    icon: vercelIcon,
+    
   },
 
   {
     slug: "docker",
     url: "https://www.docker.com/",
-    icon: dockerIcon,
+    
   },
   {
     slug: "mongodb",
     url: "https://www.mongodb.com/",
-    icon: mongodbIcon,
+    
   },
   {
     slug: "redis",
     url: "https://redis.io/",
-    icon: redisIcon,
+    
   },
   {
     slug: "digitalocean",
     url: "https://www.digitalocean.com/",
-    icon: digitaloceanIcon,
+    
   },
   {
     slug: "heroku",
     url: "https://www.heroku.com/",
-    icon: herokuIcon,
+    
   },
   {
     slug: "githubactions",
     url: "https://github.com/features/actions",
-    icon: githubactionsIcon,
+    
   },
 ];
 
@@ -200,17 +161,17 @@ export const mobileIconsTags = [
   {
     slug: "javascript",
     url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
-    icon: jsIcon,
+    
   },
   {
     slug: "typescript",
     url: "https://www.typescriptlang.org/",
-    icon: typescriptIcon,
+    
   },
   {
     slug: "react",
     url: "https://reactjs.org/",
-    icon: reactIcon,
+    
   },
   {
     slug: "reactnative",
@@ -225,12 +186,12 @@ export const mobileIconsTags = [
   {
     slug: "expo",
     url: "https://expo.dev/",
-    icon: expoIcon,
+    
   },
   {
     slug: "androidstudio",
     url: "https://developer.android.com/studio",
-    icon: androidstudioIcon,
+    
   },
   {
     slug: "reactnativepaper",
@@ -250,43 +211,43 @@ export const toolsIconTags = [
   {
     slug: "git",
     url: "https://git-scm.com/",
-    icon: gitIcon,
+    
   },
   {
     slug: "visualstudiocode",
     url: "https://code.visualstudio.com/",
-    icon: visualstudiocodeIcon,
+    
   },
   {
     slug: "jira",
     url: "https://www.atlassian.com/software/jira",
-    icon: jiraIcon,
+    
   },
   {
     slug: "figma",
     url: "https://figma.com/",
-    icon: figmaIcon,
+    
   },
 
   {
     slug: "jest",
     url: "https://jestjs.io/",
-    icon: jestIcon,
+    
   },
   {
     slug: "linux",
     url: "https://www.linux.org/",
-    icon: linuxIcon,
+    
   },
   {
     slug: "sentry",
     url: "https://sentry.io/welcome/",
-    icon: sentryIcon,
+    
   },
   {
     slug: "github",
     url: "https://github.com",
-    icon: githubIcon,
+    
   },
 ];
 
